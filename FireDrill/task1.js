48e512e0b85d25e62d9971b7d15ba620b7812c91
mysterymartial <bolasax16@gmail.com>
2024-09-13 16:17:51 +0100

    fire drill

--- FireDrill/task1.js
+++ FireDrill/task1.js
@@ -53,13 +53,102 @@ function findFirstDuplicate(numbers){
 		for(let counter = count + 1; counter < numbers.length; counter++){
 			if(numbers[count] == numbers[counter]){
 
-				return numbers[count];
-			}
+				return numbers[count]}
 		}
 	}
 	return -1;
 }
  
+function reversedArray(numbers){
+	let reversedArrays = [];
+	
+	for(let index =numbers.length-1; index >=0; index--){
+		reversedArrays.push(numbers[index]);
+
+		
+	}
+
+		return reversedArrays;
+
+		}
+function factorial(number){
+	let factorial = 1;
+	for(let count =1; count <= number; count++){
+		factorial *= count;
+}
+
+	return factorial;
+}
+
+function isPalindrome(word){
+	let reverse =""
+	
+	for(let character = word.length-1; character >= 0; character--){
+		reverse = reverse + word[character]
+		
+		
+}
+	if(word != reverse){
+		return false;
+
+}
+ 
+	else{
+		return true;
+
+	}
+
+
+	}
+
+	function sumMultipleOf3And5(number){
+		let add1 = 0;
+		let add2 = 0;
+		if(number % 3 == 0){
+		if(number % 5 == 0){
+
+		 add1 = number / 3;
+		 add2 = number /5;
+
+}
+
+}
+
+return add1 + add2;
+}
+
+function isLeapYear(year){
+
+if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+
+	return true;
+
+}
+else{
+
+	return false
+}
+
+}
+
+           
+
+				
+	
+
+	
+
+	
+	
+
+	
+
+	
+		
+		
+		
+		
+	
 
  
 
@@ -73,4 +162,4 @@ function findFirstDuplicate(numbers){
 
 
 
-module.exports = {sumOfEvenNumber,findMax,countOddNumbers,isPrime,findFirstDuplicate};
+module.exports = {sumOfEvenNumber,findMax,countOddNumbers,isPrime,findFirstDuplicate,reversedArray,factorial,isPalindrome,sumMultipleOf3And5,isLeapYear};
--- FireDrill/task1.test.js
+++ FireDrill/task1.test.js
@@ -1,4 +1,4 @@
-const {sumOfEvenNumber,findMax,countOddNumbers,isPrime,findFirstDuplicate} = require("./task1.js");
+const {sumOfEvenNumber,findMax,countOddNumbers,isPrime,findFirstDuplicate,reversedArray,factorial,isPalindrome,sumMultipleOf3And5,isLeapYear} = require("./task1.js");
 
 test("add even numbers in an array", ()=>{
 
@@ -34,17 +34,86 @@ test("check if a number is a prime number", ()=>{
 
 test("find the first duplicate in an array", ()=>{
 
-	let number1 = [1,2,3,4,5,6,2,3];
-	let result = findFirstDuplicate(number1);
+	let number = [1,2,3,4,5,6,2,3];
+	let result = findFirstDuplicate(number);
 	let expected = 2;
 	expect(result).toEqual(expected);
 });
 
 test("find that there is no duplicate in an array", ()=>{
 
-	let number1 = [1,2,3,4,5,6];
-	let result = findFirstDuplicate(number1);
+	let number = [1,2,3,4,5,6];
+	let result = findFirstDuplicate(number);
 	let expected = -1;
 	expect(result).toEqual(expected);
 });
+
+test("return a resverse of the array", ()=>{
+
+	let numbers = [1,2,3,4,5,6];
+	let result = reversedArray(numbers);
+	let expected = [6,5,4,3,2,1];
+	expect(result).toEqual(expected);
+});
+
+test("return the factorial of number", ()=>{
+
+	let number = 6;
+	let result = factorial(number);
+	let expected = 720;
+	expect(result).toBe(expected);
+});
+
+test("check if a palindrome is a string or not", ()=>{
+
+	let word = "bob";
+	let result = isPalindrome(word);
+	let expected = true;
+	expect(result).toEqual(expected);
+});
+
+test("check if a palindrome is a string or not", ()=>{
+
+	let word = "come";
+	let result = isPalindrome(word);
+	let expected = false;
+	expect(result).toEqual(expected);
+});
+
+test("check if a number is a multiple  of 3 and 5", ()=>{
+
+	let number = 30;
+	let result = sumMultipleOf3And5(number);
+	let expected = 16;
+	expect(result).toEqual(expected);
+});
+
+test("check if a year is a leap  or not", ()=>{
+
+	let year = 2023;
+	let result = isLeapYear(year);
+	let expected = false;
+	expect(result).toEqual(expected);
+});
+
+test("check if a year is a leap  or not", ()=>{
+
+	let year = 2020;
+	let result = isLeapYear(year);
+	let expected = true;
+	expect(result).toEqual(expected);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
