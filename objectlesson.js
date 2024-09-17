8494f257e39fae905b75f21ed32caa1b4519f053
mysterymartial <bolasax16@gmail.com>
2024-09-17 15:05:34 +0100

    update

---
+++ objectlesson.js
@@ -0,0 +1,107 @@
+const book = { title: "the Great Gatsby", author: "F.Scott Fitzgerald", yearPublished: 1925};
+
+function extract (book){
+	const{title,author} = book;
+	return title +  "  " + author;
+}
+
+let result = extract(book);
+console.log(result);
+
+
+function printObject(car){
+	let result = "";
+
+	for(let key in car){
+		result = result + key + ":" + car[key] + " ";
+	}
+
+
+	return result;		
+
+}
+
+function counter(){
+		
+		return{	
+			count: 0,
+			step: 1,
+			increase: function increament(){
+					this.count += this.step
+					return this.count;
+
+				},
+
+
+			decrease: function decreasement(){
+
+					this.count -= this.step
+					return this.count;
+			}
+
+			}
+			}
+
+			
+
+const human = {
+
+		firstName: "bola",
+		lastName: "shola"
+		};
+
+
+	function fullName(human){
+
+	const{firstName , lastName} = human;
+
+	return firstName + "  " + lastName;
+
+}
+
+const person = {
+
+		firstName: "bola",
+		lastName: "shola",
+		age: 25
+		};
+
+	function show(person){
+		let result = "";
+
+		for(let key in person){
+			result +=  key + ":" + person[key] + " ";
+	}
+
+
+			return result;
+		}
+			
+
+
+				
+answer = fullName(human);
+answer1 = show(person);
+console.log(answer1);
+console.log(answer);					
+
+
+
+
+
+	
+
+
+
+
+
+
+
+module.exports = {printObject}
+
+	
+	
+
+
+
+
---
+++ objectlesson.test.js
@@ -0,0 +1,26 @@
+const  {printObject}= require("./objectlesson.js");
+
+test("check if function can print a objects keys and value", ()=>{
+
+	const car = {
+			make: "Toyota",
+			model: "Camry",
+			year: 2021
+			}
+
+	let result = printObject(car);
+	let expected = "make:Toyota model:Camry year:2021 ";
+	expect(expected).toEqual(result);
+});
+
+test("check if function can print a objects keys and value", ()=>{
+
+	let count = 0;
+	let step = 1;
+	let result = counter(count,step);
+	let expected = 1;
+	expect(expected).toEqual(result);
+});
+
+
+	
