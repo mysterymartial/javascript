01a4018529daaf2f5f06ca7ccfc9dc33e04d4e39
mysterymartial <bolasax16@gmail.com>
2024-10-22 17:15:46 +0100

    update

---
+++ myPratice.js
@@ -0,0 +1,11 @@
+const user = {
+    "name": "Mystery",
+    "age": 24,
+    "occupation": "Software Engineer 
+    
+
+}
+
+const result = JSON.stringify(user)
+const result1 = JSON.parse(user)
+async/await
